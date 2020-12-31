037b3b4bfb52be9dd8e97a510c8e9876a6183521
Kyle McDowell <kymcd21@icstudents.org>
2020-12-31 05:40:41 +0000

    doesn't work, still debugging

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
+import java.util.*;
+import java.lang.Math;
+
 class Main {
   public static void main(String[] args) {
-    //for exercise 1
+    // for exercise 1
     int biggie[] = new int[10];
     for (int x = 0; x < 10; x++) {
       biggie[x] = x;
     }
     for (int y : biggie) {
-      System.out.println("element " + (y+1) + " is " + biggie[y]);
+      System.out.println("element " + (y + 1) + " is " + biggie[y]);
     }
-    //for exercise 2
-    
+    // for exercise 2
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter how long you want the arraylist to be: ");
+    int quantity=userInput.nextInt();
+    ArrayList<Integer> tortilla = new ArrayList<Integer>();
+    int z = 1;
+    while (z < quantity )
+      ;
+    {
+
+      int a=z*z;
+      tortilla.add(a);
+     
+      z++;
+    }
+     System.out.println(tortilla);
   }
 }
