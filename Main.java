63dd2f2e1186926fe5fd7b0e9afdf0456a2b5f54
Kyle McDowell <kymcd21@icstudents.org>
2020-12-31 03:27:07 +0000

    for exercise 1

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    int biggie[]=new int[10];
-    for(int x=0;x<10;x++){
-      biggie[x]=x+1;
+    //for exercise 1
+    int biggie[] = new int[10];
+    for (int x = 0; x < 10; x++) {
+      biggie[x] = x;
     }
-    for(int y=0;y<biggie.length;y++) {
-      System.out.println("element "+(y+1)+" is "+biggie[y]);
+    for (int y : biggie) {
+      System.out.println("element " + (y+1) + " is " + biggie[y]);
     }
+    //for exercise 2
+    
   }
 }
