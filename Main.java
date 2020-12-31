4fadab4184200a5c7d66d601f442830317746b1c
Kyle McDowell <kymcd21@icstudents.org>
2020-12-31 06:51:14 +0000

    output works, sum and average still broken

--- Main.java
+++ Main.java
@@ -14,18 +14,24 @@ class Main {
     // for exercise 2
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter how long you want the arraylist to be: ");
-    int quantity=userInput.nextInt();
+    int quantity = userInput.nextInt();
     ArrayList<Integer> tortilla = new ArrayList<Integer>();
-    int z = 1;
-    while (z < quantity )
-      ;
-    {
+    int z = 0;
+    int a;
+    while (z < quantity) {
 
-      int a=z*z;
+      a = z * z;
       tortilla.add(a);
-     
+
       z++;
     }
-     System.out.println(tortilla);
+    System.out.println(tortilla);
+    int sum=0;
+    for (int element : tortilla) {
+      sum = tortilla.get(element) + sum;
+    }
+    System.out.println("the sum of the values is: " + sum);
+    int average = sum / quantity;
+    System.out.println("The average of the values is: " + average);
   }
 }
